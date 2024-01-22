50df4482b33b7018021c41f2818b317e674e240b
pushkar <2022bit001@sggs.ac.in>
2024-01-23 04:04:18 +0530

    Pushkar

---
+++ Student.java
@@ -0,0 +1,13 @@
+class Pushkar_Java 
+{
+    int id;
+    String name;
+    
+public static void main(String[] args){
+    Pushkar_Java s1=new Pushkar_Java();
+    s1.id=100;
+    s1.name="pushkar";
+    System.out.println(s1.id+" "+s1.name);
+}
+
+}
---
+++ typecasting.java
@@ -0,0 +1,15 @@
+public class typecasting {
+    public static void main(String[] args) {
+        // float f=3.5f;
+        // int a=(int)f;
+        // System.out.println(a);
+        // System.out.println(f);
+        // type promotion
+        byte a=12;
+        byte b=100;
+        int result=a*b;
+        System.out.println(result);
+
+    }
+    
+}
