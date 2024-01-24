619c5cc88ac97da3dcfb9921722c8c0097583a33
pushkar <2022bit001@sggs.ac.in>
2024-01-25 03:12:36 +0530

    Addition

---
+++ Project/Task1_Add.java
@@ -0,0 +1,10 @@
+public class Task1_Add{
+	 public static void main(String[] args) {
+        int x=Addition(2,3);
+        System.out.println(x);
+    }
+    static int Addition(int a,int b){
+        return a+b;
+
+    }
+}
