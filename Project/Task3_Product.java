a44122029a2066a3c43af6616a7966aefce65c43
pushkar <2022bit001@sggs.ac.in>
2024-01-27 07:10:29 +0530

    adding task 5

--- Project/Task3_Product.java
+++
@@ -1,9 +0,0 @@
-public class Task3_Product{
-	public static void main(String[] args){
-		int x=mul(4,5);
-		System.out.println(x);
-	}
-	public static int mul(int a,int b){
-	return a*b;
-	}
-}
---
+++ Project/Task5_modulo.java
@@ -0,0 +1,9 @@
+public class Task5_modulo{
+	public static void main(String[] args){
+	int m=modulo(12,6);
+	System.out.println(m);
+	}
+	public static int modulo(int a,int b){
+	return a%b;
+	}
+}
