b737ad3b8e30af35023d021438b3feef2fadf5ba
pushkar <2022bit001@sggs.ac.in>
2024-01-31 04:55:42 +0530

    solving error

---
+++ Sample.java
@@ -0,0 +1,31 @@
+class Father{
+	int balance=230000;
+	static String surname="chavhan";
+	boolean name;
+public static void main(String[] args){
+	int balance=123;
+	Father s=new Father();
+	System.out.println(s.balance);
+	Father son=new Father();
+	System.out.println(son.balance);
+	System.out.println(s.surname);
+	son.surname="rathod";
+	System.out.println(s.surname);
+	}
+}
+class Mother{
+	int balance=230000;
+	static String surname="chavhan";
+	boolean name;
+public static void main(String[] args){
+	int balance=123;
+	Father s=new Father();
+	System.out.println(s.balance);
+	Father son=new Father();
+	System.out.println(son.balance);
+	System.out.println(s.surname);
+	son.surname="rathod";
+	System.out.println(s.surname);
+	}
+}
+
