aaf1573344ca3fe274816e9a760ccf2b421b3e27
pushkar <2022bit001@sggs.ac.in>
2024-02-20 22:26:14 +0530

    create new folder for classCodes

---
+++ ClassCode/classtask14feb.java
@@ -0,0 +1,18 @@
+class collage{
+ 	public void collage(){
+ 	System.out.println("I am in the collage");
+ 	}
+}
+class SGGS extends collage{
+	public void SGGS(){
+	System.out.println("I am in SGGS");
+	}
+	SGGS(int x)
+	public static void main(String[] args){
+	SGGS s=new SGGS();
+	s.SGGS();
+	//s.collage();
+	//System.out.println(s.SGGS);
+	}
+	
+}
---
+++ ClassCode/classtask1_14feb.java
@@ -0,0 +1,14 @@
+class father{
+	int balance=123;
+public father(float balance){
+	if(balance+0.2f==0.5f){
+	System.out.println(balance);
+	}
+	float x=balance=balance;
+	System.out.println(x);
+}
+	public static void main(String[] args){
+	father d=new father(0.3f);
+	System.out.println(d.balance);
+	}
+}
