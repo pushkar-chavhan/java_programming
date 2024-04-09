b78dd3198590401da17edb14dda1803c8853fc36
pushkar <2022bit001@sggs.ac.in>
2024-04-09 13:13:37 +0530

    Interface and EH

---
+++ ClassCode/fun.java
@@ -0,0 +1,54 @@
+interface Voter{
+	void castVote();
+}
+
+interface EC {
+	void checkVailidityOfVoter(int age) throws InvalidVoter;
+}
+
+
+class ECIndia implements Voter, EC{
+	final int minAge;
+	
+	ECIndia(int minAge){
+		this.minAge = minAge;
+	}
+
+	public void castVote(){
+		System.out.println("In CastVote:: ECIndia");	
+	}
+	
+	public void checkVailidityOfVoter(int age) throws InvalidVoter{
+		if(age < minAge){
+			throw new InvalidVoter("Invalid Voter");
+		}
+	}
+}
+
+class ECBrazil implements Voter, EC{
+	public void castVote(){
+		System.out.println("In CastVote:: ECBrazil");	
+	}
+	
+	public void checkVailidityOfVoter(int age){
+	
+		
+	}
+}
+
+
+class Election2024{
+
+	public static void main(String ...args){
+		ECIndia ecIndia = new ECIndia(10);
+		ecIndia.checkVailidityOfVoter(3);
+	}
+}
+
+
+class InvalidVoter extends RuntimeException{
+	InvalidVoter(String error){
+		super(error);
+	}
+}
+
